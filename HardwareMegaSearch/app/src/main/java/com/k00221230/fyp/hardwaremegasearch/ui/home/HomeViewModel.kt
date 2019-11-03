8e385738cc6b26b4eafb26e68f2f00415a0efa7c
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2019-11-03 19:39:07 +0000

    Restored App to Working Condition;
    No more updating Android Studio.  Sticking to version 4.0.X

--- HardwareMegaSearch/app/src/main/java/com/k00221230/fyp/hardwaremegasearch/ui/home/HomeViewModel.kt
+++ HardwareMegaSearch/app/src/main/java/com/k00221230/fyp/hardwaremegasearch/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Save money on your next upgrade!\n\nPress the button below to start a new search."
     }
     val text: LiveData<String> = _text
 }
