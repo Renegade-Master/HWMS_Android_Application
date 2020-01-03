64f68392a68545b4b035dab252048dc7c6fc664b
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2020-01-03 16:07:55 +0000

    Created new project;
    - HWMS/*
      - Created new Android Project.

---
+++ HWMS/app/src/main/java/com/k00221230/fyp/hwms/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.k00221230.fyp.hwms.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
